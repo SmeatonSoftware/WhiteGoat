21698fdca32495fb3fb448fcbd8e9d9f751ac2f8
Oscar Davies <oscar.william.davies@outlook.com>
2023-08-14 11:06:48 +0100

    Can Provide Data For Change Page

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -8,23 +8,29 @@ export default class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {page: 0};
+        this.state = {page: 0, data: {}};
+    }
+
+    changePage(_page = 0, _data = {}){
+        this.setState({page: _page, data: _data});
     }
 
     render() {
         var that = this;
 
-        var _selectedPage = <Welcome pageChange={(p) => that.setState({page: p})}/>;
+        var cPage = (p,d)=>that.changePage(p,d);
+
+        var _selectedPage = <Welcome pageChange={cPage}/>;
 
         switch (this.state.page) {
             case 0:
-                _selectedPage = <Welcome pageChange={(p) => that.setState({page: p})}/>;
+                _selectedPage = <Welcome pageChange={cPage}/>;
                 break;
             case 1:
-                _selectedPage = <Browse pageChange={(p) => that.setState({page: p})}/>;
+                _selectedPage = <Browse pageChange={cPage}/>;
                 break;
             case 2:
-                _selectedPage = <About pageChange={(p) => that.setState({page: p})}/>;
+                _selectedPage = <About pageChange={cPage}/>;
                 break;
         }
 
